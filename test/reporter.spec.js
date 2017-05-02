60b54c34f7d085ce91b295be9a02cca508dde5df
André Cruz <andremiguelcruz@msn.com>
2017-05-02 14:53:43 +0100

    Remove colors from snapshot testing.

--- test/reporter.spec.js
+++ test/reporter.spec.js
@@ -1,49 +1,19 @@
 'use strict';
 
 const fs = require('fs');
-const WritableStream = require('stream').Writable;
-const escapeRegExp = require('lodash.escaperegexp');
 const webpackIsomorphicCompiler = require('../');
 const createCompiler = require('./util/createCompiler');
+const createOutputStream = require('./util/createOutputStream');
 const configBasicClient = require('./configs/basic-client');
 const configBasicServer = require('./configs/basic-server');
 const configSyntaxError = require('./configs/syntax-error');
-
-function createInMemoryOutputStream() {
-    let output = '';
-    const writableStream = new WritableStream();
-
-    return Object.assign(writableStream, {
-        _write(chunk, encoding, callback) {
-            output += chunk;
-            callback();
-        },
-
-        getOutput() {
-            return output;
-        },
-
-        getReportOutput() {
-            return output
-            // Replace (xxxms) with (10ms)
-            .replace(/\(\d+ms\)/g, '(10ms)')
-            // Remove any file sizes
-            .replace(/\d+\.\d+\skB/g, 'x.xx kB')
-            // Remove absolute directory references
-            .replace(new RegExp(escapeRegExp(process.cwd()), 'g'), '')
-            // Normalize stack traces done by pretty-error
-            .replace(new RegExp(`${escapeRegExp('    [0m')}.+`, 'g'), '    [stack]')
-            .replace(/(\s{4}\[stack\])([\s\S]+\[stack\])*/, '$1');
-        },
-    });
-}
 
 describe('reporter', () => {
     afterEach(() => createCompiler.teardown());
 
     it('should report a successful compilation', () => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
 
         return compiler.run({
             report: { output: outputStream },
@@ -55,7 +25,7 @@ describe('reporter', () => {
 
     it('should only display stats in the first compilation if options.stats = \'once\'', (done) => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
         let callsCount = 0;
 
         return compiler.watch({
@@ -74,7 +44,7 @@ describe('reporter', () => {
 
     it('should not display stats if options.stats = false', (done) => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
         let callsCount = 0;
 
         return compiler.watch({
@@ -93,7 +63,7 @@ describe('reporter', () => {
 
     it('should report a failed compilation', () => {
         const compiler = createCompiler(configBasicClient, configSyntaxError);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
 
         return compiler.run({
             report: { output: outputStream },
@@ -106,7 +76,7 @@ describe('reporter', () => {
 
     it('should report a fatal error while compiling', () => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
         const contrivedError = new Error('foo');
 
         compiler.client.webpackCompiler.plugin('before-run', (compiler, callback) => {
@@ -124,7 +94,7 @@ describe('reporter', () => {
 
     it('should report a ENOENT error while compiling, hiding the stack', () => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
         const contrivedError = Object.assign(new Error('foo'), { code: 'ENOENT', hideStack: true });
 
         compiler.client.webpackCompiler.plugin('before-run', (compiler, callback) => {
@@ -142,7 +112,7 @@ describe('reporter', () => {
 
     it('should report a TypeError error while compiling', () => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
         const contrivedError = Object.assign(new TypeError('foo'));
 
         compiler.client.webpackCompiler.plugin('before-run', (compiler, callback) => {
@@ -160,7 +130,7 @@ describe('reporter', () => {
 
     it('should use options.statsOptions when printing stats', () => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
 
         return compiler.run({
             report: { stats: true, statsOptions: { hash: true }, output: outputStream },
@@ -172,7 +142,7 @@ describe('reporter', () => {
 
     it('should dispose the created reporter if .run() throws', () => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
 
         const promise = compiler.run({
             report: { stats: true, statsOptions: { hash: true }, output: outputStream },
@@ -190,7 +160,7 @@ describe('reporter', () => {
 
     it('should dispose the created reporter if .watch() throws', (done) => {
         const compiler = createCompiler(configBasicClient, configBasicServer);
-        const outputStream = createInMemoryOutputStream();
+        const outputStream = createOutputStream();
 
         compiler.watch({
             report: { stats: true, statsOptions: { hash: true }, output: outputStream },
